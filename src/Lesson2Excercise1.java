59ca2d5c22df60c93684b2e240e01053304f0960
30183161 <30183161@nescol.ac.uk>
2023-09-18 11:37:28 +0100

    asd

--- src/Lesson2Excercise1.java
+++ src/Lesson2Excercise1.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Lesson2Excercise1 {
     public static void main(String[] args) {
-        System.out.println("Here are 5 9-letter anagrams:\nFUN PIE ROB = ibuprofen\nA MOON SCAR = macaroons\nCANVAS ICE = vacancies\nEAT FABRIC = fabricate\nADDS HUNCH = dachshund");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.next();
+        System.out.print("Enter your hobby: ");
+        String hobby = input.next();
+        System.out.println(name + " loves " + hobby + " in their free time.\nThey find it relaxing and satisfying to see their "
+                + hobby + " come to life.\n" + hobby + " allows " + name + " to express their creative side.");
     }
 }
