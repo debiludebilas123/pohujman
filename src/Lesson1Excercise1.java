bb258315769335817299efb325c22740d61d4305
30183161 <30183161@nescol.ac.uk>
2023-09-18 11:30:19 +0100

    asd

---
+++ src/Lesson1Excercise1.java
@@ -0,0 +1,5 @@
+public class Lesson1Excercise1 {
+    public static void main(String[] args) {
+        System.out.println("Heat wave scorches land\nCrops wilt under blazing sun\nClimate change marches on");
+    }
+}
