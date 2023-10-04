75d0623e7e1ae89177d01e03cb66c2afeb4c3a4c
30183161 <30183161@nescol.ac.uk>
2023-10-04 09:31:39 +0100

    asd

---
+++ src/Lesson6Exercise1.java
@@ -0,0 +1,9 @@
+public class Lesson6Exercise1 {
+    public static void main(String[] args) {
+        int num = 10;
+        while (num <= 24) {
+            System.out.println(num);
+            num+=2;
+        }
+    }
+}
