5932a45b79abdc1b40ca65a99bd1a55cb2f45f22
30183161 <30183161@nescol.ac.uk>
2023-11-01 12:03:53 +0000

    asd

---
+++ src/Lesson8Exercise1.java
@@ -0,0 +1,8 @@
+public class Lesson8Exercise1 {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Kiausas", "kiausiai", "desra", "Bonifacijus", "Bezdzione"};
+        System.out.println(names[0]);
+        System.out.println(names[4]);
+        System.out.println("The number of items in the array is: "+names.length);
+    }
+}
