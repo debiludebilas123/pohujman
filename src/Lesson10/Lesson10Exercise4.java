7dc79ddca3aa8992d8cfad9f39a779c627e9a9f9
30183161 <30183161@nescol.ac.uk>
2023-12-06 13:59:37 +0000

    asd

--- src/Lesson10/Lesson10Exercise4.java
+++ src/Lesson10/Lesson10Exercise4.java
@@ -1,7 +1,34 @@
 package Lesson10;
 
-public class Lesson10Exercise4 {
-    public static void main(String[] args) {
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lesson10Exercise4 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type 2 text file names one after another here: ");
+        String textFile1 = scanner.nextLine();
+        String textFile2 = scanner.nextLine();
+
+        BufferedReader in = new BufferedReader(new FileReader(textFile1));
+        BufferedReader in1 = new BufferedReader(new FileReader(textFile2));
+
+        while (true) {
+            String line = in.readLine();
+            String line1 = in1.readLine();
+
+            if (line == null || line1 == null) {
+                break;
+            }
+
+            if (!line.equals(line1)) {
+                System.out.println("The contents are different.");
+                break;
+            }
+        }
+        System.out.println("The contents are the same.");
     }
 }
